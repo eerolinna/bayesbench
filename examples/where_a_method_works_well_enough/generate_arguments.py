4f34dd303d72557eb7106dd7f61570b5f7727ff6
Eero Linna <eero.linna@gmail.com>
2019-03-15 11:21:58 +0200

    Fix

--- examples/where_a_method_works_well_enough/generate_arguments.py
+++ examples/where_a_method_works_well_enough/generate_arguments.py
@@ -5,6 +5,6 @@ knowledge_base = bayesbench.load_knowledge_base("kb_location")
 
 new_kb = bayesbench.filter_knowledge_base(
     knowledge_base, methods=["stan_vi_fullrank", "stan_nuts"]
-)  # TODO right now gold standards are implicit,
+)
 
 bayesbench.generate_arguments_file("arguments.json", knowledge_db=new_kb)
