a230f13f82cfbca218e7050e0ab00f494420b5a0
Eero Linna <eero.linna@gmail.com>
2019-03-29 11:28:23 +0200

    Add a custom inference method example

---
+++ stan_api_inference_engine.py
@@ -0,0 +1,16 @@
+# Currently this function name must be `inference`
+def inference(model, data, diagnostics, seed, extra_fitting_args):
+    """Here the model is stan API for model so we can access log likelihood and its gradients and the parameterization of the model
+    
+    Right now this takes multiple arguments but we can also have it take one argument (`config`) that contains everything needed
+    
+    TODO: this should fill default values with explicit values. Thus this should return not only the inference result but also the explicit fitting_args used
+    """
+    pass
+
+
+def validate_args(args):
+    # We can use something like JSON schema for validating these
+
+    # This could probably be optional, at least for user-defined custom inference methods. If the user doesn't include validation function then the inputs just won't be validated beforehand.
+    pass
