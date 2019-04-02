6174b78b6066aa8852fe72a86c11eca0620b75b6
Eero Linna <eero.linna@gmail.com>
2019-04-02 18:16:13 +0300

    Extract inference engines out from run.py

--- bayes_benchmark/run.py
+++ bayes_benchmark/run.py
@@ -4,13 +4,6 @@
 # - method to use: nuts / hmc / vi-fullrank / vi-meanfield
 # - output directory
 
-# compile model with stan_utility
-# run inference
-# save output file
-
-# Saving output file
-# - figure out filename. we take hash of model code (file contents) + method name
-
 # Write some code for loading also
 # - load all outputs from directory to list
 # - or load some specified output (give model file and give method name, or list of these)
@@ -24,116 +17,115 @@
 # - Should be pretty straightforward to adapt from stan code
 
 import json
-from typing import Dict, Any, Mapping
+from typing import Dict, Any, Mapping, Sequence
 from .output import Output
-from . import stan_utility
-import pystan
+from .posterior_db import PosteriorDatabase
 import functools
 import numpy
 from hashlib import md5
 import os
 import time
 import importlib
-import pymc3 as pm
+import argparse
 
 
 def run(
     *,
-    model_code_path: str,
-    method_name: str,
-    lib_name: str,
-    dataset_path: str,
+    posterior_db_location: str,
+    inference_engine: str,
+    model_name: str,
+    dataset_name: str,
+    extra_fitting_args: Mapping[str, Any],
+    diagnostics: Sequence[str],
     output_dir: str,
+    seed: int = None,
 ) -> None:
-    if lib_name == "stan":
-        run_stan(
-            model_code_path=model_code_path,
-            dataset_path=dataset_path,
-            output_dir=output_dir,
-            method_name=method_name,
-        )
-    else:
-        raise NotImplementedError()
 
+    # load model from posterior DB
+    # - Stan: load model as text
+    # - PyMC: Use importlib to import the python module
+    # Maybe both of these should happen in inference engine
 
-def stan_method(*, stan_model, method_name):
-    "method names should not contain dashes (-)"
-    stan_methods: Mapping[str, Any] = {
-        "stan_nuts": stan_model.sampling,
-        "stan_vb_fullrank": functools.partial(stan_model.vb, algorithm="fullrank"),
-        "stan_vb_meanfield": functools.partial(stan_model.vb, algorithm="meanfield"),
-    }
-    return stan_methods[method_name]
+    # load dataset from posterior DB
 
+    # load inference engine
 
-def run_stan(
-    *, model_code_path: str, dataset_path: str, output_dir: str, method_name: str
-) -> None:
-    """
-    Run inference and save output
-    """
+    # load diagnostics with importlib
+
+    # run inference engine with model and dataset and extra fitting args
+    # inference engine returns samples and diagnostic values
+    # Create output
+    #
+
+    posterior_db = PosteriorDatabase(posterior_db_location)
+
+    dataset_path = posterior_db.get_dataset_path(dataset_name)
+
+    inference_engine_name, method_name = inference_engine.rsplit(".", 1)
+
     with open(dataset_path) as json_f:
         dataset = json.load(json_f)
 
-    model_code = open(model_code_path).read()
+    inference_engine_module = importlib.import_module(inference_engine_name)
 
-    # compile stan model
-    # give it dataset
-    stan_model = stan_utility.compile_model(model_code_path)
+    method = getattr(inference_engine_module, method_name)
 
     start = time.time()
 
-    # check if method X should be skipped for model Y
-    method = stan_method(stan_model=stan_model, method_name=method_name)
-
-    # load extra args for model X and method Y
-    extra_args: Dict[str, Any] = {}
-
-    stan_fit = method(data=dataset, **extra_args)
-
-    # run diagnostics
-    # check that diagnostic and method are compatible
+    samples, diagnostic_values, explicit_args = method(
+        model_name=model_name,
+        data=dataset,
+        diagnostics=diagnostics,
+        get_model_path=posterior_db.get_model_path,
+        seed=seed,
+        extra_fitting_args=extra_fitting_args,
+    )
 
     end = time.time()
-    output = stan_result_to_output(
-        stan_fit=stan_fit,
+    output = result_to_output(
+        samples=samples,
+        diagnostics=diagnostic_values,
+        inference_engine_name=inference_engine_name,
         creation_time=start,
         execution_time=end - start,
-        model_name=model_code_path,
+        model_name=model_name,
         dataset_path=dataset_path,
         method_name=method_name,
     )
-    # TODO file for extra_fitting_args
 
     save_output(
         output=output,
         output_dir=output_dir,
-        model_code=model_code,
+        inference_engine=inference_engine_name,
+        model_name=model_name,
         method_name=method_name,
-        dataset_path=dataset_path,
+        dataset_name=dataset_name,
     )
 
 
-def stan_result_to_output(
-    *, stan_fit, creation_time, execution_time, model_name, dataset_path, method_name
+def result_to_output(
+    *,
+    samples,
+    diagnostics,
+    creation_time,
+    execution_time,
+    model_name,
+    dataset_path,
+    method_name,
+    inference_engine_name,
 ):
-    """
-    Extract samples
-    Not sure if this needs to be a function
-    """
-    samples = stan_fit.extract(permuted=True)
-
     output = Output(
         samples=samples,
-        diagnostics=[],
+        diagnostics=diagnostics,
         creation_time=creation_time,
         execution_time=execution_time,
         model_name=model_name,
         lang="python",
-        framework="stan",
+        inference_engine=inference_engine_name,
         dataset_name=dataset_path,
         extra_fitting_args={},
         method_name=method_name,
+        seed=None,
     )
     return output
 
@@ -145,19 +137,29 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
+def get_hash(to_hash: str) -> str:
+    return md5(to_hash.encode("ascii")).hexdigest()
+
+
 def save_output(
     *,
     output: Output,
     output_dir: str,
-    model_code: str,
+    inference_engine: str,
+    model_name: str,
     method_name: str,
-    dataset_path: str,
+    dataset_name: str,
 ) -> None:
-    code_hash = md5(model_code.encode("ascii")).hexdigest()
+    # filename can be inference engine name - method name - dataset name - model name
+    inference_engine_hash = get_hash(inference_engine)
 
-    dataset_hash = md5(dataset_path.encode("ascii")).hexdigest()
+    model_hash = get_hash(model_name)
 
-    filename = f"{method_name}-{code_hash}-{dataset_hash}.json"
+    dataset_hash = get_hash(dataset_name)
+
+    method_hash = get_hash(method_name)
+
+    filename = f"{inference_engine_hash}-{method_hash}-{model_hash}-{dataset_hash}.json"
 
     full_filename = os.path.join(output_dir, filename)
 
@@ -165,61 +167,54 @@ def save_output(
         json.dump(output.to_dict(), f, cls=NumpyEncoder)
 
 
-"""
+def main():
+    parser = argparse.ArgumentParser(description="Run inference")
+    parser.add_argument("--json", required=True, help="Arguments as json string")
+    parser.add_argument("--output-dir", required=True, help="Output directory")
 
-import pymc3 as pm
+    args = parser.parse_args()
 
-X, y = linear_training_data()
-with pm.Model() as linear_model:
-    weights = pm.Normal('weights', mu=0, sd=1)
-    noise = pm.Gamma('noise', alpha=2, beta=1)
-    y_observed = pm.Normal('y_observed',
-                mu=X.dot(weights),
-                sd=noise,
-                observed=y)
+    data = json.loads(args.json)
 
-    prior = pm.sample_prior_predictive()
-    posterior = pm.sample()
-    posterior_pred = pm.sample_posterior_predictive(posterior)
+    model_name = data["model_name"]
+    inference_engine = data["inference_engine"]
+    dataset_name = data["dataset_name"]
+    output_dir = args.output_dir
+    posterior_db_location = data["posterior_db_location"]
+    extra_fitting_args = data.get("extra_fitting_args", {})
+    diagnostics = data.get("diagnostics", [])
+
+    run(
+        model_name=model_name,
+        inference_engine=inference_engine,
+        dataset_name=dataset_name,
+        output_dir=output_dir,
+        posterior_db_location=posterior_db_location,
+        extra_fitting_args=extra_fitting_args,
+        diagnostics=diagnostics,
+    )
 
 
-pymc_model_path
-
-"""
-
-
-def run_pymc(
-    *, model_code_path: str, dataset_path: str, output_dir: str, method_name: str
-) -> None:
-    model_module = importlib.import_module(
-        model_code_path
-    )  # TODO might need to process path
-    model = model_module.model  # type: ignore
-
-    with open(dataset_path) as json_f:
-        dataset = json.load(json_f)
-
-    model_with_data = model(**dataset)
-
-    result = pm.sample(model=model_with_data)
-
-    # can also use pm.ADVI(model=model_with_data)
-    # or pm.FullRankADVI(model=model_with_data)
-    # these need pm.fit() also
-    ...
+def test_main():
+    # load command line arguments here and pass to run
+    model_name = "8_schools_noncentered"
+    inference_engine = "bayesbench_stan.nuts"
+    dataset_name = "8_schools"
+    output_dir = "out"
+    posterior_db_location = "/home/eero/default_posterior_db"
+    extra_fitting_args: Mapping[str, Any] = {}
+    diagnostics: Sequence[str] = []
+    run(
+        model_name=model_name,
+        inference_engine=inference_engine,
+        dataset_name=dataset_name,
+        output_dir=output_dir,
+        posterior_db_location=posterior_db_location,
+        extra_fitting_args=extra_fitting_args,
+        diagnostics=diagnostics,
+    )
+    print("Done")
 
 
 if __name__ == "__main__":
-    model_code_path = "8_schools.stan"
-    method_name = "stan_vb_fullrank"
-    lib_name = "stan"
-    dataset_path = "8_schools_data.json"
-    output_dir = "out"
-    run(
-        model_code_path=model_code_path,
-        method_name=method_name,
-        lib_name=lib_name,
-        dataset_path=dataset_path,
-        output_dir=output_dir,
-    )
-    print("Done")
+    main()
