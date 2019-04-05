880c955e12d04a82aa88bf271495c2646d44ad4a
Eero Linna <eero.linna@gmail.com>
2019-04-05 15:53:20 +0300

    Change bayesbench into a pip installable package

---
+++ bayesbench/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="bayesbench",
+    version="0.1",
+    description="Benchmark Bayesian inference methods",
+    url="http://github.com/bayesbench/bayesbench",
+    author="Eero Linna",
+    author_email="eero.linna@aalto.fi",
+    license="MIT",
+    packages=["bayesbench"],
+    zip_safe=False,
+)
+
