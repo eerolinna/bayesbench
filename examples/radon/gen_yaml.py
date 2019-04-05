1ad933d16fea76a694110905feeef81627f96cc9
Eero Linna <eero.linna@gmail.com>
2019-04-05 15:45:29 +0300

    Update radon config generation

--- examples/radon/gen_yaml.py
+++ examples/radon/gen_yaml.py
@@ -1,18 +1,21 @@
 import yaml
 import os
 
-model_names = """radon_hiearchical_intercept2_hoprior
+posterior_names = """radon_hiearchical_intercept2_hoprior
+radon_mn_hiearchical_intercept2_hoprior
 radon_partial_pooling2_hoprior
+radon_mn_partial_pooling2_hoprior
 radon_variable_intercept_slope2_hoprior
+radon_mn_variable_intercept_slope2_hoprior
 radon_hiearchical_intercept2
+radon_mn_hiearchical_intercept2
 radon_partial_pooling2
-radon_variable_intercept_slope2""".split(
+radon_mn_partial_pooling2
+radon_variable_intercept_slope2
+radon_mn_variable_intercept_slope2""".split(
     "\n"
 )
 
-for m in model_names:
-    print(m)
-dataset_names = ["radon", "radon_mn"]
 methods = [
     "bayesbench_stan.meanfield_vi",
     "bayesbench_stan.fullrank_vi",
@@ -25,24 +28,22 @@ tolerances = [0.1, 0.01, 0.001]
 
 n_iterations_list = [100, 1000, 10000]
 
-for model_name in model_names:
-    for dataset_name in dataset_names:
-        for method in methods:
-            for tolerance in tolerances:
-                for n_iterations in n_iterations_list:
-                    runs.append(
-                        {
-                            "model_name": model_name,
-                            "dataset_name": dataset_name,
-                            "inference_engine": method,
-                            "posterior_db_location": "db_loc",
-                            "diagnostics": ["psis_khat"],
-                            "extra_fitting_arguments": {
-                                "tolerance": tolerance,
-                                "n_iterations": n_iterations,
-                            },
-                        }
-                    )
+for posterior_name in posterior_names:
+    for method in methods:
+        for tolerance in tolerances:
+            for n_iterations in n_iterations_list:
+                runs.append(
+                    {
+                        "posterior_name": posterior_name,
+                        "inference_engine": method,
+                        "posterior_db_location": "db_loc",
+                        "diagnostics": ["psis_khat"],
+                        "method_specific_arguments": {
+                            "tolerance": tolerance,
+                            "n_iterations": n_iterations,
+                        },
+                    }
+                )
 
 yaml_contents = yaml.dump(runs)
 
