68b2181cb23c7fc7520da508a5125d6cdb517ad2
Eero Linna <eero.linna@gmail.com>
2019-04-09 14:08:30 +0300

    Turn bayesbench_stan into a pip installable package

---
+++ bayesbench_stan/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="bayesbench_stan",
+    version="0.1",
+    description="Bayesbench interface to Stan",
+    url="http://github.com/bayesbench/bayesbench",
+    author="Eero Linna",
+    author_email="eero.linna@aalto.fi",
+    license="MIT",
+    packages=["bayesbench_stan"],
+    zip_safe=False,
+)
+
