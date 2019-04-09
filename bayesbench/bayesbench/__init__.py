bbb50a051d326fcf71fd8f784e2dfedfd948317d
Eero Linna <eero.linna@gmail.com>
2019-04-09 16:52:12 +0300

    Add function to compare posterior means of outputs

--- bayesbench/bayesbench/__init__.py
+++ bayesbench/bayesbench/__init__.py
@@ -1,3 +1,4 @@
 from . import run
 from . import output
 from . import posterior_db
+from . import compare_means
---
+++ bayesbench/bayesbench/compare_means.py
@@ -0,0 +1,51 @@
+from bayesbench.posterior_db import PosteriorDatabase
+from collections import defaultdict
+from typing import Sequence, Mapping, List, Any
+from bayesbench.output import Output
+import json
+import numpy as np
+
+
+def compare_means(outputs: Sequence[Output], posterior_db_location=None):
+    result: Mapping[str, List[Any]] = defaultdict(list)
+    for output in outputs:
+        posterior_name = output.run_config.posterior_name
+
+        means = get_mean(output)
+
+        result[posterior_name].append(
+            {
+                "method": output.run_config.method_name,
+                "inference_engine": output.run_config.inference_engine,
+                "means": means,
+                "gold_standard": False,
+            }
+        )
+
+    if posterior_db_location:
+        for posterior_name in result:
+            posterior_db = PosteriorDatabase(posterior_db_location)
+
+            gold_standard = posterior_db.load_gold_standard(posterior_name)
+
+            if gold_standard:
+                posterior_name = gold_standard.run_config.posterior_name
+                means = get_mean(gold_standard)
+                result[posterior_name].append(
+                    {
+                        "method": gold_standard.run_config.method_name,
+                        "inference_engine": gold_standard.run_config.inference_engine,
+                        "means": means,
+                        "gold_standard": True,
+                    }
+                )
+    return result
+
+
+def get_mean(output: np.Array):
+    result = {}
+    for key in output.samples:
+        values = output.samples[key]
+        result[key] = values.mean(axis=0)
+
+    return result
