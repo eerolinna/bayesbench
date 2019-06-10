1b5a2460b073cde5f4cd1fa5f8dcbab4f25b4fa5
Eero Linna <eero.linna@gmail.com>
2019-06-10 11:39:50 +0300

    Add zip file output processing

--- bayesbench/bayesbench/output.py
+++ bayesbench/bayesbench/output.py
@@ -1,11 +1,21 @@
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, fields
 from typing import Any, Sequence, Mapping, Tuple, Optional
 import numpy as np
+from zipfile import ZipFile
+import json
 
 Samples = Mapping[str, Sequence[float]]
 
 # Maybe better to change output format into a zipfile that contains 1 file for metadata and 1 file for samples.
 # Then we can quickly load the metadata file without having to load the sample file
+
+"""
+@dataclass
+class Samples:
+    posterior: Mapping[str, Sequence[float]]
+    posterior_predictive: Mapping[str, Sequence[float]]
+    log_likelihood: Sequence[float]
+"""
 
 
 @dataclass
@@ -23,6 +33,14 @@ class RunConfig:
     @classmethod
     def from_dict(cls, dct):
         return cls(**dct)
+
+    @classmethod
+    def from_zip(cls, filename: str):
+        with ZipFile(filename) as zip:
+            with zip.open("config.json") as config_f:
+                run_config_dict = json.load(config_f)
+
+        return cls.from_dict(run_config_dict)
 
 
 @dataclass
@@ -50,12 +68,52 @@ class Output:
     @classmethod
     def from_dict(cls, dct):
         dct["run_config"] = RunConfig.from_dict(dct["run_config"])
+
+        """
+        Equivalent to
+        output = Output(
+            run_config=dct["run_config"],
+            samples=dct["samples"],
+            creation_time=dct["creation_time"],
+            ...
+        )
+        """
         output = cls(**dct)
 
         new_samples = {k: np.array(v) for (k, v) in output.samples.items()}
         output.samples = new_samples
         return output
 
+    def to_zip(self, filename: str):
+        with ZipFile(filename, "w") as zip:
+            config = json.dumps(self.run_config)
+            zip.writestr("config.json", config)
+
+            field_names = [
+                field.name for field in fields(self) if field.name is not "run_config"
+            ]
+            # This is equivalent to
+            # {
+            #   "samples": self.samples,
+            #   "creation_time": self.creation_time,
+            #   ...
+            # }
+            rest = {field_name: getattr(self, field_name) for field_name in field_names}
+
+            zip.writestr("inference.json", json.dumps(rest))
+
+    @classmethod
+    def from_zip(cls, filename: str):
+        with ZipFile(filename) as zip:
+            with zip.open("config.json") as config_f:
+                run_config_dict = json.load(config_f)
+
+            with zip.open("inference.json") as f:
+                dct = json.load(f)
+
+        dct["run_config"] = run_config_dict
+        return cls.from_dict(dct)
+
 
 # Diagnostic output needs to be JSON serializable
-# Extra_fitting_args also need to be JSON serializable
+# Method specific arguments also need to be JSON serializable
