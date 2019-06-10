8278b0f8411f789c2e3e66e1e15b4d0e4eb3c91f
Eero Linna <eero.linna@gmail.com>
2019-06-10 11:42:48 +0300

    Revert a commit to use the old PDB structure for now
    Revert "WIP on updating to new PDB structure"
    This reverts commit 06ecb5086e82a9333dc562022ff4a4944851f5f5.

--- bayesbench/bayesbench/posterior_db.py
+++ bayesbench/bayesbench/posterior_db.py
@@ -14,20 +14,8 @@ class PosteriorDatabase:
         self.location = location
         self.posteriors = get_posteriors(location)
 
-
-    def get_model_path(self, *, posterior_name: str, framework: str) -> Optional[str]:
-        """Obtains model file location for a given posterior and framework"""
-        posterior = self.posteriors[posterior_name]
-        relative_path = posterior["model"][framework]
-        absolute_path = join(self.location, relative_path)
-        return absolute_path
-
-    def get_model_path_raw(self, *, model_name, framework, file_extension) -> Optional[str]:
-        """This is currently not ported to new PDB structure. Instead there is a
-        separate function for it. However that function can't do everything
-        this one can so TODO do something with this
-        """
-        model_dir = join(self.location, join("content", "models"))
+    def get_model_path(self, *, model_name, framework, file_extension) -> Optional[str]:
+        model_dir = join(self.location, "models")
         paths = glob.glob(
             model_dir + f"/**/{model_name}{file_extension}", recursive=True
         )
@@ -43,10 +31,11 @@ class PosteriorDatabase:
         return model_name
 
     def get_dataset_path(self, *, posterior_name):
-        """Obtains dataset file location for `posterior_name`"""
-        dataset_relative_path = self.posteriors[posterior_name]["data"]
-        dataset_absolute_path = join(self.location, dataset_relative_path)
-        return dataset_absolute_path
+        dataset_name = self.posteriors[posterior_name]["dataset_name"]
+        dataset_dir = join(self.location, "datasets")
+        paths = glob.glob(dataset_dir + f"/**/{dataset_name}.json", recursive=True)
+        assert len(paths) == 1, f"There were multiple datasets named {dataset_name}"
+        return paths[0]
 
     def print_posteriors(self):
         posteriors_dir = join(self.location, "posteriors")
@@ -80,13 +69,13 @@ class PosteriorDatabase:
 
 def get_posteriors(location):
     posteriors_dir = join(location, "posteriors")
-    paths = glob.glob(posteriors_dir + "/**/*.json", recursive=True)
+    paths = glob.glob(posteriors_dir + "/**/*.yaml", recursive=True)
     all_posteriors = {}
     for path in paths:
         with open(path) as f:
-            posterior = json.load(f)
-            file_name = os.path.basename(path)
-            posterior_name = os.path.splitext(file_name)[0]
-            all_posteriors[posterior_name] = posterior
+            posteriors = yaml.safe_load(f)
+            for posterior in posteriors:
+                posterior_name = posterior["posterior_name"]
+                all_posteriors[posterior_name] = posterior
 
     return all_posteriors
