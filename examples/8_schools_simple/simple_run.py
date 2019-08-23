117fa7abdb392dd9145da23dc47d61a6dad604f5
Eero Linna <eero.linna@aalto.fi>
2019-08-23 16:18:22 +0300

    Update simple_run

--- examples/8_schools_simple/simple_run.py
+++ examples/8_schools_simple/simple_run.py
@@ -1,36 +1,38 @@
 import bayesbench
 
-posterior_names = ["8_schools|noncentered"]
-methods = ["fullrank", "meanfield"]
 
-runs = []
+def main():
+    posterior_names = ["8_schools-8_schools_noncentered"]
+    methods = ["fullrank", "meanfield"]
 
-tolerances = [0.01]
+    runs = []
 
-n_iterations_list = [1000]
+    tolerances = [0.01]
 
-for posterior_name in posterior_names:
-    for method in methods:
-        for tolerance in tolerances:
-            for n_iterations in n_iterations_list:
-                runs.append(
-                    {
-                        "posterior_name": posterior_name,
-                        "inference_engine": "bayesbench_stan.vb",
-                        "diagnostics": [],
-                        "posterior_db_location": "/home/eero/posterior_db",
-                        "output_dir": "temp_out",
-                        "method_specific_arguments": {
-                            "tol_rel_obj": tolerance,
-                            "iter": n_iterations,
-                            "algorithm": method,
-                        },
-                    }
-                )
+    n_iterations_list = [1000]
+
+    for posterior_name in posterior_names:
+        for method in methods:
+            for tolerance in tolerances:
+                for n_iterations in n_iterations_list:
+                    runs.append(
+                        {
+                            "posterior_name": posterior_name,
+                            "inference_engine": "bayesbench_stan.vb",
+                            "diagnostics": [],
+                            "posterior_db_location": "/home/eero/posterior_database",
+                            "output_dir": "temp_out",
+                            "method_specific_arguments": {
+                                "tol_rel_obj": tolerance,
+                                "iter": n_iterations,
+                                "algorithm": method,
+                            },
+                        }
+                    )
 
 
-outputs = bayesbench.run.run_many(runs)
+    outputs = bayesbench.run.run_many(runs, parallel=False)
 
-# result = bayesbench.benchmark(outputs)
+    # result = bayesbench.benchmark(outputs)
 
-# print(result)
+    # print(result)
